f5a6689c5c72a93718c8b5cca45cb0d6e56ac646
Nathaniel Putera <nathaniel.putera@nathaniels-mini.gdn-commerce.com>
2019-03-27 11:41:54 +0700

    Initial Commit

---
+++ Food/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Nathaniel Putera on 27/03/19.
+//  Copyright © 2019 Nathaniel Putera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
