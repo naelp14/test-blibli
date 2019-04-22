c5fdb6c7e8ab52b5acf53ad5edae6e64d1d75c36
Nathaniel Putera <nathaniel.putera@nathaniels-mini.gdn-commerce.com>
2019-04-22 15:01:32 +0700

    second commit

--- Food/ViewController.swift
+++ Food/ViewController.swift
@@ -7,14 +7,104 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var desc: UILabel!
+    
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Cannot convert UIImage to CIImage")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
+    func detect (image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Failed to load model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print(results)
+            if let firstResult = results.first {
+                let str = "\(firstResult.identifier)"
+                if str.contains(","){
+                    let r = str.index(str.startIndex, offsetBy: 0)..<str.index(str.firstIndex(of: ",")!, offsetBy: 0)
+                    self.navigationItem.title = String(str[r]).capitalized
+                    self.request(imageName: String(str[r]))
+                } else {
+                    self.navigationItem.title = firstResult.identifier.capitalized
+                    self.request(imageName: firstResult.identifier)
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    func request (imageName: String) {
+        
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "description",
+            "titles" : imageName,
+            "indexpageids" : "",
+            "redirects" : "1",
+            ]
+        
+        Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.result.isSuccess {
+                print(response)
+                
+                let tempJSON: JSON = JSON(response.result.value!)
+                let pageid = tempJSON["query"]["pageids"][0].stringValue
+                let temp = tempJSON["query"]["pages"][pageid]["description"].stringValue
+                
+                self.desc.text = temp
+                
+            }
+        }
 
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
